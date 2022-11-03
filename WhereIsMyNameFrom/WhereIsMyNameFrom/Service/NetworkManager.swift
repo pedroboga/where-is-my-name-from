2785d07219f0d0246108911d4b727415cc4768d2
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-11-03 16:23:53 -0300

    Created network layer

---
+++ WhereIsMyNameFrom/WhereIsMyNameFrom/Service/NetworkManager.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkManager.swift
+//  WhereIsMyNameFrom
+//
+//  Created by Pedro Boga on 03/11/22.
+//
+
+import Foundation
+
+enum ApiError: Error {
+    case invalidUrl
+    case invalidRequest
+}
+
+class NetworkManager {
+    private let originURL = ""
+    
+    func nameOrigin(name: String) async throws -> Origin {
+        guard let url = URL(string: originURL + name) else {
+            throw ApiError.invalidUrl
+        }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(Origin.self, from: data)
+        } catch {
+            throw ApiError.invalidRequest
+        }
+    }
+}
